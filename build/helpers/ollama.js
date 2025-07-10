02c05c24d4dddd54edae8388618cdc872c1ffc21
Angel Isaac Fonseca Gómez <90788511+Angelfonseca@users.noreply.github.com>
2025-07-10 09:56:59 -0600

    Delete build directory

--- build/helpers/ollama.js
+++
@@ -1,8 +0,0 @@
-import ollama from 'ollama';
-export const getEmbedding = async (prompt) => {
-    const response = await ollama.embeddings({
-        model: 'llama3',
-        prompt,
-    });
-    return response.embedding;
-};
